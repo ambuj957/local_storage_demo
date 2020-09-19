e0524403b7e61ec03049b446abd5708f5f707473
Ambuj Singh <ambuj.singh@aricent.com>
2020-09-19 17:12:18 +0530

    local storage

--- src/App.js
+++ src/App.js
@@ -2,25 +2,43 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+	constructor(props) {
+    super(props);
+    this.state = {
+      val: localStorage.getItem('key'),
+    }
+
+  }
+	
+	save=(e)=>{
+		localStorage.setItem("key", this.state.val);
+		alert('Saved');
+	}
+	remove=(e)=>{
+		localStorage.removeItem("key");
+		alert('Removed');
+	}
+	show=(e)=>{
+		let value = localStorage.getItem("key");
+		this.setState({val:value});
+		alert('Stored value is ' + value);
+	}
+	render(){
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+	        <input type="text" value={this.state.val} onChange={(event)=>{
+				this.setState({val:event.target.value});
+			}
+			}/>
+            <button onClick={this.save}>Save</button>
+			<button onClick={this.remove}>Remove</button>
+			<button onClick={this.show}>Retrieve</button>
       </header>
     </div>
   );
+	}
 }
 
 export default App;
